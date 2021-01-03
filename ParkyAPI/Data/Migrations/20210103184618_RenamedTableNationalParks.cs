c7eb599cf0a183dc3d54b53512949dfc3ffd2727
Sladjana6 <sladjanafilipovic6@gmail.com>
2021-01-03 21:23:25 +0100

    Add project files.

---
+++ ParkyAPI/Data/Migrations/20210103184618_RenamedTableNationalParks.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkyAPI.Migrations
+{
+    public partial class RenamedTableNationalParks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NationalParks",
+                table: "NationalParks");
+
+            migrationBuilder.RenameTable(
+                name: "NationalParks",
+                newName: "NationalPark");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NationalPark",
+                table: "NationalPark",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NationalPark",
+                table: "NationalPark");
+
+            migrationBuilder.RenameTable(
+                name: "NationalPark",
+                newName: "NationalParks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NationalParks",
+                table: "NationalParks",
+                column: "Id");
+        }
+    }
+}
