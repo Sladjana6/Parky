3e7379246f4433ec7eb0e4c74ed509f4555b1c40
Sladjana6 <sladjanafilipovic6@gmail.com>
2021-03-03 15:28:22 +0100

    Proba sa usign

--- ParkyAPI/Models/Dtos/TrailDto.cs
+++ ParkyAPI/Models/Dtos/TrailDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Configuration;
 using System.Threading.Tasks;
 using static ParkyAPI.Models.Trail;
 
